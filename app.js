87c95c6252db8fc44cfc96b4782f62add704ec4a
ZikriAzzuri5 <41791670+ZikriAzzuri5@users.noreply.github.com>
2022-03-05 18:50:17 +0700

    add cors module

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const cors = require("cors");
 
 // import mongoose
 const mongoose = require("mongoose");
@@ -34,7 +35,7 @@ app.use(
   })
 );
 app.use(flash());
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
